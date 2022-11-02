d1c00bf9720e4f4ddaaa555dc76a7ddce642c8e0
seokunee <xocoolree@gmail.com>
2022-11-03 01:47:39 +0900

    project페이지 편집

--- data/projectsData.js
+++ data/projectsData.js
@@ -1,19 +1,16 @@
 const projectsData = [
   {
-    title: 'A Search Engine',
-    description: `What if you could look up any information in the world? Webpages, images, videos
-    and more. Google has many features to help you find exactly what you're looking
-    for.`,
-    imgSrc: '/static/images/google.png',
-    href: 'https://www.google.com',
+    title: 'Running Mate',
+    description: `같이 달리실래요?" 함께 달리는 플랫폼 RunningMate`,
+    imgSrc:
+      'https://www.notion.so/image/https%3A%2F%2Fs3-us-west-2.amazonaws.com%2Fsecure.notion-static.com%2F699d3ec3-d2c0-415f-8278-d7907e5aad75%2F%E1%84%89%E1%85%B3%E1%84%8F%E1%85%B3%E1%84%85%E1%85%B5%E1%86%AB%E1%84%89%E1%85%A3%E1%86%BA_2022-06-04_%E1%84%8B%E1%85%A9%E1%84%92%E1%85%AE_9.03.58.png?table=block&id=fe28cb65-36e0-414e-a7a3-f84763ee6eff&spaceId=7000a91d-0c9a-4399-8790-020012848543&width=2000&userId=7ad068da-2997-4375-b77c-f3375286953c&cache=v2',
+    href: 'https://www.notion.so/Running-Mate-fe28cb6536e0414ea7a3f84763ee6eff',
   },
   {
-    title: 'The Time Machine',
-    description: `Imagine being able to travel back in time or to the future. Simple turn the knob
-    to the desired date and press "Go". No more worrying about lost keys or
-    forgotten headphones with this simple yet affordable solution.`,
-    imgSrc: '/static/images/time-machine.jpg',
-    href: '/blog/the-time-machine',
+    title: '유튜브 클론 프로젝트',
+    description: `React 와 YOUTUBE API를 가지고 YOUTUBE의 기능을 구현해보는 클론 코딩`,
+    imgSrc: 'https://images.roa.ai/articles/2020/02/youtube.png',
+    href: 'https://www.notion.so/YOUTUBE-CLONE-62d44a55307c41c4b8edb78fb2089166',
   },
 ]
 
